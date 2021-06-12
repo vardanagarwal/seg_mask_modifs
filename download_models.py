7d61314c2fb6115f89ea16d8fd12461a7e30e7c4
vardanagarwal <vardanagarwal16@gmail.com>
2021-06-13 04:28:16 +0530

    Updated download and label files

--- download_models.py
+++ download_models.py
@@ -3,57 +3,52 @@ import torch
 import torchvision
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
-class download_models:
-    """Helper class to download models"""
+def maskrcnn_coco(save_path='models/maskrcnn_restnet50_fpn.pt'):
+    """ Download and save maskrcnn model
 
-    def maskrcnn_coco(self, save_path='models/maskrcnn_restnet50_fpn.pt'):
-        """ Download and save maskrcnn model
+    Arguments:
+    save_path: Path to save maskrcnn model. Must end with '.pt'. Default: 'models/maskrcnn_restnet50_fpn.pt'
+    """
 
-        Arguments:
-        save_path: Path to save maskrcnn model. Must end with '.pt'. Default: 'models/maskrcnn_restnet50_fpn.pt'
-        """
+    if save_path[-3:] != '.pt':
+        raise ValueError('Save path should end with .pt')
 
-        if save_path[-3:] != '.pt':
-            raise ValueError('Save path should end with .pt')
+    if save_path == 'models/maskrcnn_restnet50_fpn.pt' and not os.path.exists('models'):
+        os.makedirs('models')
 
-        if save_path == 'models/maskrcnn_restnet50_fpn.pt' and not os.path.exists('models'):
-            os.makedirs('models')
+    # getting base model from pytorch torchvision
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+    torch.save(model, 'models/maskrcnn_resnet50_fpn.pt')
 
-        # getting base model from pytorch torchvision
-        model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-        torch.save(model, 'models/maskrcnn_resnet50_fpn.pt')
+def deeplab_pascal(save_path='models/deeplab_restnet101.pt'):
+    """ Download and save deeplab model
 
-    def deeplab_pascal(self, save_path='models/deeplab_restnet101.pt'):
-        """ Download and save deeplab model
+    Arguments:
+    save_path: Path to save deeplab model. Must end with '.pt'. Default: 'models/deeplab_restnet101.pt'
+    """
 
-        Arguments:
-        save_path: Path to save deeplab model. Must end with '.pt'. Default: 'models/deeplab_restnet101.pt'
-        """
+    if save_path[-3:] != '.pt':
+        raise ValueError('Save path should end with .pt')
 
-        if save_path[-3:] != '.pt':
-            raise ValueError('Save path should end with .pt')
+    if save_path == 'models/deeplab_restnet101.pt' and not os.path.exists('models'):
+        os.makedirs('models')
 
-        if save_path == 'models/deeplab_restnet101.pt' and not os.path.exists('models'):
-            os.makedirs('models')
+    # getting base model from pytorch torchvision
+    model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=True)
+    torch.save(model, 'models/deeplab_restnet101.pt')
 
-        # getting base model from pytorch torchvision
-        model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=True)
-        torch.save(model, 'models/deeplab_restnet101.pt')
+def face(save_path='models/face.pth'):
+    """ Download and save face model
 
-    def face(self, save_path='models/face.pth'):
-        """ Download and save face model
+    Arguments:
+    save_path: Path to save face model. Must end with '.pth'. Default: 'models/face.pth'
+    """
 
-        Arguments:
-        save_path: Path to save face model. Must end with '.pth'. Default: 'models/face.pth'
-        """
+    if save_path[-3:] != '.pth':
+        raise ValueError('Save path should end with .pth')
 
-        if save_path[-3:] != '.pth':
-            raise ValueError('Save path should end with .pth')
+    if save_path == 'models/face.pth' and not os.path.exists('models'):
+        os.makedirs('models')
 
-        if save_path == 'models/face.pth' and not os.path.exists('models'):
-            os.makedirs('models')
-
-        gdd.download_file_from_google_drive(file_id='154JgKpzCPW82qINcVieuPH3fZ2e0P812',
-                                            dest_path='models/face.pth')
-
-    
+    gdd.download_file_from_google_drive(file_id='154JgKpzCPW82qINcVieuPH3fZ2e0P812',
+                                        dest_path='models/face.pth')
